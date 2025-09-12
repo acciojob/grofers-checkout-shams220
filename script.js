15646b8c6ca26c888a56539a2fa89626c2ad4eed
shams alam <103259015+shams220@users.noreply.github.com>
2025-09-12 08:56:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+let arr = document.querySelectorAll(".price");
+	let sum = 0;
+arr.forEach((el,i)=>{
+	sum+=Number(el.innerText);
+})
+	
+let row  = document.createElement("tr");
+let col1 = document.createElement("td");
+let col2 = document.createElement("td");
+col1.innerText = "Total";
+col2.innerText = Number(sum);
+col1.classList.add("item");
+col2.classList.add("price");
+row.appendChild(col1);
+	row.appendChild(col2);
+	document.querySelector("table tbody").appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
