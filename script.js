9aad217a7f0de0d3a5e0d78cb133b9dbae39a232
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-23 10:17:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
-let arr = document.querySelectorAll(".price");
-	let sum = 0;
-arr.forEach((el,i)=>{
-	sum+=Number(el.innerText);
-})
-	
-let row  = document.createElement("tr");
-let col1 = document.createElement("td");
-let col2 = document.createElement("td");
-col1.innerText = "Total";
-col2.innerText = Number(sum);
-col1.classList.add("item");
+const prices = document.querySelectorAll('.prices')
+const total = [...prices].reduce((a,b)=>{
+	return a+ Number(b.textContent);
+},0)
+
+const row = document.createElement('tr');
+const col1 = document.createElement("td");
+const col2 = document.createElement('td');
+
+row.classList.add("item");
+col1.classList.add("price");
 col2.classList.add("price");
-row.appendChild(col1);
-	row.appendChild(col2);
-	document.querySelector("table tbody").appendChild(row);
-};
 
-getSumBtn.addEventListener("click", getSum);
+row.appendChild(col1)
+row.appendChild(col2)
+
+col1.textContent("Total sum");
+col2.textContent(total);
+
+document.querySelector('table').appendChild(row);
+
+
 
