82826060e94b1e34f288f5ba37b3ffa49c794e10
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-23 17:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-const prices = document.querySelectorAll('.price');
+const sumbutton = document.createElement("button");
+sumbutton.textContent = "find sum";
+sumbutton.classList.add("sumbtn");
 
-const sumBtn = document.createElement('button');
-sumBtn.classList.add('sumBtn');
-sumBtn.textContent = "Click me";
-document.body.appendChild(sumBtn); 
+const row = document.createElement('tr');
+const col1 = document.createElement('td');
+const col2 = document.createElement('td');
 
-const sumFn = () => {
-  const total = [...prices].reduce((a, b) => a + Number(b.textContent), 0);
+const tablebody = document.querySelector('tbody');
 
-  const row = document.createElement('tr');
-  const col1 = document.createElement("td");
-  const col2 = document.createElement('td');
+row.appendChild(col1);
+row.appendChild(col2);
+tablebody.appendChild(row)
+tablebody.appendChild(sumbutton)
+col1.textContent = "total";
 
-  col1.classList.add("item");
-  col2.classList.add("price");
 
-  col1.textContent = "Total sum";
-  col2.textContent = total;
+const prices = document.querySelectorAll('.price')
+let total = 0;
+sumbutton.addEventListener('click',()=>{
+	 total = [...prices].reduce((a,b)=>{
+	return a+Number(b.textContent);
+},0)
+ col2.textContent = total;
+	
+})
 
-  row.appendChild(col1);
-  row.appendChild(col2);
 
-  document.querySelector('table').appendChild(row);
-};
 
-sumBtn.addEventListener('click', sumFn);
+// const prices = document.querySelectorAll('.price');
+
+// const sumBtn = document.createElement('button');
+// sumBtn.classList.add('sumBtn');
+// sumBtn.textContent = "Click me";
+// document.body.appendChild(sumBtn); 
+
+// const sumFn = () => {
+//   const total = [...prices].reduce((a, b) => a + Number(b.textContent), 0);
+
+//   const row = document.createElement('tr');
+//   const col1 = document.createElement("td");
+//   const col2 = document.createElement('td');
+
+//   col1.classList.add("item");
+//   col2.classList.add("price");
+
+//   col1.textContent = "Total sum";
+//   col2.textContent = total;
+
+//   row.appendChild(col1);
+//   row.appendChild(col2);
+
+//   document.querySelector('table').appendChild(row);
+// };
+
+// sumBtn.addEventListener('click', sumFn);
